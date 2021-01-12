b656bff64016f39e05c398c18e24e409519fc781
MOMO Adrien Nickson <mani@ekwali.local>
2021-01-12 16:48:53 +0100

    Autors added

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonItem, IonLabel, IonAvatar } from '@ionic/react';
+
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 import PostsContainer from "../components/PostsContainer";
@@ -18,7 +20,38 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">...</IonTitle>
           </IonToolbar>
         </IonHeader>
-
+          <IonContent>
+              <IonItem>
+                  <IonAvatar slot="start">
+                      <img src="assets/img/Channel.jpg" />
+                  </IonAvatar>
+                  <IonLabel>
+                      <h2>DONKENG Channel</h2>
+                      <h3>channel-laetitia.donkeng-donfack@ionis-stm.com</h3>
+                      <p>M1 Info</p>
+                  </IonLabel>
+              </IonItem>
+              <IonItem>
+                  <IonAvatar slot="start">
+                      <img src="assets/img/Lyna.jpg" />
+                  </IonAvatar>
+                  <IonLabel>
+                      <h2>TAMOUD Lyna</h2>
+                      <h3>lyna.tamoud@ionis-stm.com</h3>
+                      <p>M1 Info</p>
+                  </IonLabel>
+              </IonItem>
+              <IonItem>
+                  <IonAvatar slot="start">
+                      <img src="assets/img/Adrien.jpg" />
+                  </IonAvatar>
+                  <IonLabel>
+                      <h2>MOMO Adrien Nickson</h2>
+                      <h3>adrien-nickson.momo@ionis-stm.com</h3>
+                      <p>M1 Info</p>
+                  </IonLabel>
+              </IonItem>
+          </IonContent>
       </IonContent>
     </IonPage>
   );
